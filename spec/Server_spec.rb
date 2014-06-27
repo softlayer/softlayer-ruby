a26bdd8099659a78ecc43d35ec8d1580087a8a25
Scott Thompson <sthompson@softlayer.com>
2014-06-27 12:12:04 -0500

    Added badges to readme and removed unused portions of a server spect test

--- spec/Server_spec.rb
+++ spec/Server_spec.rb
@@ -15,12 +15,6 @@ require 'rspec'
 describe SoftLayer::Server do
 	it "is an abstract base class" do
 		mock_client = SoftLayer::Client.new(:username => "fakeuser", :api_key => "DEADBEEFBADF00D")
-		allow(mock_client).to receive(:[]) do |service_name|
-			service = mock_client.service_named(service_name)
-			allow(service).to receive(:call_softlayer_api_with_params)
-			service
-		end
-
 		expect { SoftLayer::Server.new(mock_client, { "id" => 12345 }) }.to raise_error
 	end
 end
