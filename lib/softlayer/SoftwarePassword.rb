3eeb0da4ee338fa26a9bad3affd17399fba41d34
Julio Lajara <ju2wheels@gmail.com>
2014-11-19 12:57:50 -0500

    Add find software passwords on hardware and virtual server methods to SoftwarePassword

--- lib/softlayer/SoftwarePassword.rb
+++ lib/softlayer/SoftwarePassword.rb
@@ -283,7 +283,270 @@ module SoftLayer
     end
 
     ##
-    # Returns the service for interacting with this software component passowrd through the network API
+    # Retrieve a list of software passwords from software on hardware devices.
+    #
+    # The options parameter should contain:
+    #
+    # <b>+:client+</b> - The client used to connect to the API
+    #
+    # If no client is given, then the routine will try to use Client.default_client
+    # If no client can be found the routine will raise an error.
+    #
+    # You may filter the list returned by adding options:
+    # * <b>+:datacenter+</b>    (string) - Include software passwords from software on hardware matching this datacenter
+    # * <b>+:description+</b>   (string) - Include software passwords from software that matches this description
+    # * <b>+:domain+</b>        (string) - Include software passwords from software on hardware matching this domain
+    # * <b>+:hardware_type+</b> (string) - Include software passwords from software on hardware matching this hardware type
+    # * <b>+:hostname+</b>      (string) - Include software passwords from software on hardware matching this hostname
+    # * <b>+:manufacturer+</b>  (string) - Include software passwords from software that matches this manufacturer
+    # * <b>+:name+</b>          (string) - Include software passwords from software that matches this name
+    # * <b>+:username+</b>      (string) - Include software passwords for username matching this username
+    #
+    # You may use the following properties to provide hardware or software object filter instances:
+    # * <b>+:hardware_object_filter+</b>          (ObjectFilter) - Include software passwords from software on hardware that matches the criteria of this object filter
+    # * <b>+:software_object_filter+</b>          (ObjectFilter) - Include software passwords from software that matches the criteria of this object filter
+    # * <b>+:software_password_object_filter*</b> (ObjectFilter) - Include software passwords that match the criteria of this object filter
+    # * <b>+:software_password_object_mask+</b>   (string)       - Include software password properties that matches the criteria of this object mask
+    #
+    def self.find_passwords_for_software_on_hardware(options_hash = {})
+      softlayer_client = options_hash[:client] || Client.default_client
+      raise "#{__method__} requires a client but none was given and Client::default_client is not set" if !softlayer_client
+
+      if(options_hash.has_key? :hardware_object_filter)
+        hardware_object_filter = options_hash[:hardware_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless hardware_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        hardware_object_filter = ObjectFilter.new()
+      end
+
+      if(options_hash.has_key? :software_object_filter)
+        software_object_filter = options_hash[:software_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless software_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        software_object_filter = ObjectFilter.new()
+      end
+
+      if(options_hash.has_key? :software_password_object_filter)
+        software_password_object_filter = options_hash[:software_password_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless software_password_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        software_password_object_filter = ObjectFilter.new()
+      end
+
+      filter_label = {
+        :bare_metal_instance => "bareMetalInstances",
+        :hardware            => "hardware",
+        :network_hardware    => "networkHardware",
+        :router              => "routers"
+      }
+
+      option_to_filter_path = {
+        :datacenter        => lambda { |hardware_type| return [ filter_label[hardware_type], '.datacenter.name' ].join        },
+        :domain            => lambda { |hardware_type| return [ filter_label[hardware_type], '.domain' ].join                 },
+        :hostname          => lambda { |hardware_type| return [ filter_label[hardware_type], '.hostname' ].join               },
+        :tags              => lambda { |hardware_type| return [ filter_label[hardware_type], '.tagReferences.tag.name' ].join },
+        :software          => {
+          :description     => "softwareComponents.softwareDescription.longDescription",
+          :manufacturer    => "softwareComponents.softwareDescription.manufacturer",
+          :name            => "softwareComponents.softwareDescription.name",
+          :username        => "softwareComponents.passwords.username"
+        },
+        :software_password => {
+          :username        => "passwords.username"
+        }
+      }
+
+      if options_hash[:hardware_type]
+        unless filter_label.keys.include?(options_hash[:hardware_type])
+          raise "Expected :bare_metal_instance, :hardware, :network_hardware, or :router for option :hardware_type in #{__method__}"
+        end
+      end
+
+      [ :datacenter, :domain, :hostname ].each do |option|
+        if options_hash[option]
+          hardware_object_filter.modify { |filter| filter.accept(option_to_filter_path[option].call(options_hash[:hardware_type] || :hardware)).when_it is(options_hash[option]) }
+        end
+      end
+
+      if options_hash[:tags]
+        hardware_object_filter.set_criteria_for_key_path(option_to_filter_path[:tags].call(options_hash[:hardware_type] || :hardware),
+                                                         {
+                                                           'operation' => 'in',
+                                                           'options' => [{
+                                                                           'name' => 'data',
+                                                                           'value' => options_hash[:tags].collect{ |tag_value| tag_value.to_s }
+                                                                         }]
+                                                         })
+      end
+
+      option_to_filter_path[:software].each do |option, filter_path|
+        software_object_filter.modify { |filter| filter.accept(filter_path).when_it is(options_hash[option]) } if options_hash[option]
+      end
+
+      option_to_filter_path[:software_password].each do |option, filter_path|
+        software_password_object_filter.modify { |filter| filter.accept(filter_path).when_it is(options_hash[option]) } if options_hash[option]
+      end
+
+      account_service = softlayer_client[:Account]
+      account_service = account_service.object_filter(hardware_object_filter) unless hardware_object_filter.empty?
+      account_service = account_service.object_mask("mask[id]")
+
+      case options_hash[:hardware_type]
+      when :bare_metal_instance
+        hardware_data = account_service.getBareMetalInstances
+      when :hardware, nil
+        hardware_data = account_service.getHardware
+      when :network_hardware
+        hardware_data = account_service.getNetworkHardware
+      when :router
+        hardware_data = account_service.getRouters
+      end
+
+      software_passwords = hardware_data.collect do |hardware|
+        hardware_service = softlayer_client[:Hardware].object_with_id(hardware['id'])
+        hardware_service = hardware_service.object_filter(software_object_filter) unless software_object_filter.empty?
+        hardware_service = hardware_service.object_mask("mask[id]")
+
+        software_data    = hardware_service.getSoftwareComponents
+
+        software_data.collect do |software|
+          next if software.empty?
+
+          software_service = softlayer_client[:Software_Component].object_with_id(software['id'])
+          software_service = software_service.object_filter(software_password_object_filter) unless software_password_object_filter.empty?
+          software_service = software_service.object_mask(SoftwarePassword.default_object_mask)
+          software_service = software_service.object_mask(options_hash[:software_password_object_mask]) if options_hash[:software_password_object_mask]
+
+          software_passwords_data = software_service.getPasswords
+          software_passwords_data.map { |password| SoftwarePassword.new(softlayer_client, password) unless password.empty? }.compact
+        end
+      end
+
+      software_passwords.flatten
+    end
+
+    ##
+    # Retrieve a list of software  passwords from software virtual servers.
+    #
+    # The options parameter should contain:
+    #
+    # <b>+:client+</b> - The client used to connect to the API
+    #
+    # If no client is given, then the routine will try to use Client.default_client
+    # If no client can be found the routine will raise an error.
+    #
+    # You may filter the list returned by adding options:
+    # * <b>+:datacenter+</b>    (string) - Include software passwords from software on virtual servers matching this datacenter
+    # * <b>+:description+</b>   (string) - Include software passwords from software that matches this description
+    # * <b>+:domain+</b>        (string) - Include software passwords from software on virtual servers matching this domain
+    # * <b>+:hostname+</b>      (string) - Include software passwords from software on virtual servers matching this hostname
+    # * <b>+:manufacturer+</b>  (string) - Include software passwords from software that matches this manufacturer
+    # * <b>+:name+</b>          (string) - Include software passwords from software that matches this name
+    # * <b>+:username+</b>      (string) - Include software passwords for username matching this username
+    #
+    # You may use the following properties to provide virtual server or software object filter instances:
+    # * <b>+:virtual_server_object_filter+</b>    (ObjectFilter) - Include software passwords from software on virtual servers that matches the criteria of this object filter
+    # * <b>+:software_object_filter+</b>          (ObjectFilter) - Include software passwords from softwarethat matches the criteria of this object filter
+    # * <b>+:software_password_object_filter*</b> (ObjectFilter) - Include software passwords that match the criteria of this object filter
+    # * <b>+:software_password_object_mask+</b>   (string)       - Include software password properties that matches the criteria of this object mask
+    #
+    def self.find_passwords_for_software_on_virtual_servers(options_hash = {})
+      softlayer_client = options_hash[:client] || Client.default_client
+      raise "#{__method__} requires a client but none was given and Client::default_client is not set" if !softlayer_client
+
+      if(options_hash.has_key? :virtual_server_object_filter)
+        virtual_server_object_filter = options_hash[:virtual_server_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless virtual_server_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        virtual_server_object_filter = ObjectFilter.new()
+      end
+
+      if(options_hash.has_key? :software_object_filter)
+        software_object_filter = options_hash[:software_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless software_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        software_object_filter = ObjectFilter.new()
+      end
+
+      if(options_hash.has_key? :software_password_object_filter)
+        software_password_object_filter = options_hash[:software_password_object_filter]
+        raise "Expected an instance of SoftLayer::ObjectFilter" unless software_password_object_filter.kind_of?(SoftLayer::ObjectFilter)
+      else
+        software_password_object_filter = ObjectFilter.new()
+      end
+
+      option_to_filter_path = {
+        :software          => {
+          :description     => "softwareComponents.softwareDescription.longDescription",
+          :manufacturer    => "softwareComponents.softwareDescription.manufacturer",
+          :name            => "softwareComponents.softwareDescription.name",
+          :username        => "softwareComponents.passwords.username"
+        },
+        :virtual_server    => {
+          :datacenter      => "virtualGuests.datacenter.name",
+          :domain          => "virtualGuests.domain",
+          :hostname        => "virtualGuests.hostname",
+          :tags            => "virtualGuests.tagReferences.tag.name"
+        },
+        :software_password => {
+          :username        => "passwords.username"
+        }
+      }
+
+      if options_hash[:tags]
+        virtual_server_object_filter.set_criteria_for_key_path(option_to_filter_path[:virtual_server][:tags],
+                                                                {
+                                                                  'operation' => 'in',
+                                                                  'options' => [{
+                                                                                  'name' => 'data',
+                                                                                  'value' => options_hash[:tags].collect{ |tag_value| tag_value.to_s }
+                                                                                }]
+                                                                })
+      end
+
+      option_to_filter_path[:virtual_server].each do |option, filter_path|
+        next if option == :tags
+        virtual_server_object_filter.modify { |filter| filter.accept(filter_path).when_it is(options_hash[option]) } if options_hash[option]
+      end
+
+      option_to_filter_path[:software].each do |option, filter_path|
+        software_object_filter.modify { |filter| filter.accept(filter_path).when_it is(options_hash[option]) } if options_hash[option]
+      end
+
+      option_to_filter_path[:software_password].each do |option, filter_path|
+        software_password_object_filter.modify { |filter| filter.accept(filter_path).when_it is(options_hash[option]) } if options_hash[option]
+      end
+
+      account_service = softlayer_client[:Account]
+      account_service = account_service.object_filter(virtual_server_object_filter) unless virtual_server_object_filter.empty?
+      account_service = account_service.object_mask("mask[id]")
+
+      virtual_server_data = account_service.getVirtualGuests
+
+      software_passwords = virtual_server_data.collect do |virtual_server|
+        virtual_server_service = softlayer_client[:Virtual_Guest].object_with_id(virtual_server['id'])
+        virtual_server_service = virtual_server_service.object_filter(software_object_filter) unless software_object_filter.empty?
+        virutal_server_service = virtual_server_service.object_mask("mask[id]")
+
+        software_data          = virtual_server_service.getSoftwareComponents
+        software_data.collect do |software| 
+          next if software.empty?
+
+          software_service = softlayer_client[:Software_Component].object_with_id(software['id'])
+          software_service = software_service.object_filter(software_password_object_filter) unless software_password_object_filter.empty?
+          software_service = software_service.object_mask(SoftwarePassword.default_object_mask)
+          software_service = software_service.object_mask(options_hash[:software_password_object_mask]) if options_hash[:software_password_object_mask]
+
+          software_passwords_data = software_service.getPasswords
+          software_passwords_data.map { |password| SoftwarePassword.new(softlayer_client, password) unless password.empty? }.compact
+        end
+      end
+
+      software_passwords.flatten
+    end
+
+    ##
+    # Returns the service for interacting with this software component password through the network API
     #
     def service
       softlayer_client[:Software_Component_Password].object_with_id(self.id)
